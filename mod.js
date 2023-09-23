7578b2f619eeddec72bd89e52a928a5c9965aa39
packjack <83289186+Packjackisback@users.noreply.github.com>
2023-09-23 16:07:10 -0500

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,6 +1,6 @@
 setInterval(function() {
   var layout = ovoModAPI.game.getLayout();
   if (layout.name == "Level 41") {
-    c2_callFunction("Menu > End", []);
+    c2_callFunction("Menu > EndGame", []);
   }
 }, 100);
